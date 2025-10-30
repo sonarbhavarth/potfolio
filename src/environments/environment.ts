fa3f0234456cc11a4c3d9992ba8ca0816305210d
bhavarth sonar <sonarbhavarth@gmail.com>
2025-10-30 12:40:01 +0530

    revert: restore environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "<YOUR_FIREBASE_API_KEY>",
-    authDomain: "<YOUR_PROJECT_ID>.firebaseapp.com",
-    projectId: "<YOUR_PROJECT_ID>",
-    storageBucket: "<YOUR_PROJECT_ID>.firebasestorage.app",
-    messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
-    appId: "<YOUR_APP_ID>",
-    measurementId: "<YOUR_MEASUREMENT_ID>"
+    apiKey: "AIzaSyAX1qmiutkX4mcpARpzdg8stcE8O_Br2Hs",
+    authDomain: "potfolio-f41c1.firebaseapp.com",
+    projectId: "potfolio-f41c1",
+    storageBucket: "potfolio-f41c1.firebasestorage.app",
+    messagingSenderId: "93452761326",
+    appId: "1:93452761326:web:616c49377ea7a9a175dbff",
+    measurementId: "G-2TBSZ45ZST"
   },
-  geminiApiKey: "<YOUR_GEMINI_API_KEY>"
+  geminiApiKey: "AIzaSyCnFsKTnYE2yVWOTuEue1DRI4kjQNjjzi8"
 };
